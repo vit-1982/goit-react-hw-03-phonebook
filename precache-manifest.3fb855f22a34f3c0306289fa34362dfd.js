1a78194b77602d9e0df1d14e33159923a25db13f
Vitalii Zelenyi <vzntst@gmail.com>
2020-10-18 22:35:00 +0300

    Updates

---
+++ precache-manifest.3fb855f22a34f3c0306289fa34362dfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67bd69eef912a6b4f695de8d640143f0",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "83ebf11a6dc180d7cce8",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.c6ec3d8b.chunk.css"
+  },
+  {
+    "revision": "c435d3bbc5ef75ff4703",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.041aef5a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.041aef5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83ebf11a6dc180d7cce8",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.a98cc855.chunk.js"
+  },
+  {
+    "revision": "159f9e7c304b75423aa5",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.cca09c81.js"
+  }
+]);
